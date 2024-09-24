760affd3a2e8a14d7a458f842285b4a04e1dd3df
Joshua Kite <joshuamkite@gmail.com>
2024-09-24 10:16:16 +0100

    successfull deployments

--- README.md
+++ README.md
@@ -1,16 +1,45 @@
+# CDK Python AWS AutoScaling Group
 
-# Welcome to your CDK Python project!
+- [CDK Python AWS AutoScaling Group](#cdk-python-aws-autoscaling-group)
+  - [Prerequisites](#prerequisites)
+  - [Setup](#setup)
+    - [Install the dependencies:](#install-the-dependencies)
+    - [Set Up Environment Variables](#set-up-environment-variables)
+    - [Deployment](#deployment)
+      - [Deploy the Stack](#deploy-the-stack)
+    - [Destroy the Stack](#destroy-the-stack)
+  - [Useful commands](#useful-commands)
 
-This is a blank project for CDK development with Python.
+This project is an AWS CDK (Cloud Development Kit) stack written in Python that deploys an AutoScaling group, load balancer, and optionally a Route53 DNS record. Serves 'Hello World' webpage. It uses environment variables for configuration and requires manual setup for account-specific details.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Prerequisites
 
-This project is set up like a standard Python project.  The initialization
-process also creates a virtualenv within this project, stored under the `.venv`
-directory.  To create the virtualenv it assumes that there is a `python3`
-(or `python` for Windows) executable in your path with access to the `venv`
-package. If for any reason the automatic creation of the virtualenv fails,
-you can create the virtualenv manually.
+Before using this project, ensure you have the following installed:
+
+- [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html)
+- [Node.js](https://nodejs.org/en/download/) (version 14.x or higher)
+- [AWS CDK](https://docs.aws.amazon.com/cdk/latest/guide/getting_started.html) (installed globally)
+
+Install AWS CDK globally:
+
+```bash
+npm install -g aws-cdk
+```
+
+## Setup
+
+### Install the dependencies:
+
+
+
+
+
+
+
+
+
+
+create the virtualenv manually.
 
 To manually create a virtualenv on MacOS and Linux:
 
@@ -37,15 +66,71 @@ Once the virtualenv is activated, you can install the required dependencies.
 $ pip install -r requirements.txt
 ```
 
-At this point you can now synthesize the CloudFormation template for this code.
 
+
+
+
+
+### Set Up Environment Variables
+
+To configure environment-specific settings, you'll need to create and fill out the appropriate `.env` files.
+
+1. **Default `.env.dns` for DNS configurations:**
+
+   If you want to enable Route53 DNS record creation, the `.env.dns` file has the following structure:
+
+   ```plaintext
+   TAGS='[{"key": "Name", "value": "cdk-demo"}, {"key": "environment", "value": "production"}]'
+   CREATE_DNS_RECORD=true
+   ```
+
+2. **Default `.env.notdns` for configurations without DNS:**
+
+   You can also use `.env.notdns` if you don't want to create Route53 DNS records, TLS certificate etc.:
+
+   ```plaintext
+   TAGS='[{"key": "Name", "value": "cdk-demo"}, {"key": "environment", "value": "development"}]'
+   CREATE_DNS_RECORD=false
+   ```
+
+I find it simpler to export environment variables directly for the common parts, e.g. `source .env.common` where `.env.common` is like:
+
+```bash
+export CDK_DEFAULT_ACCOUNT=
+export CDK_DEFAULT_REGION=
+export AWS_PROFILE=
+export AWS_REGION=
+export DNS_NAME="" # Only required if CREATE_DNS_RECORD=true
+export HOSTED_ZONE_ID='' # Only required if CREATE_DNS_RECORD=true
+export ZONE_NAME='' # Only required if CREATE_DNS_RECORD=true
 ```
+ 
+### Deployment
+
+At this point you can now synthesize the CloudFormation template for this code (optional).
+
+```bash
 $ cdk synth
 ```
 
-To add additional dependencies, for example other CDK libraries, just add
-them to your `setup.py` file and rerun the `pip install -r requirements.txt`
-command.
+
+#### Deploy the Stack
+
+To deploy the stack to your AWS account:
+
+```bash
+   cdk deploy
+```
+
+This will deploy your resources, including AutoScaling groups, security groups, load balancers, and (optionally) DNS records if the `.env.dns` file is configured.
+
+### Destroy the Stack
+
+To tear down all the resources created by this stack:
+
+```bash
+   cdk destroy
+```
 
 ## Useful commands
 
--- cdk_python_aws_autoscaling_group/cdk_python_aws_autoscaling_group_stack.py
+++ cdk_python_aws_autoscaling_group/cdk_python_aws_autoscaling_group_stack.py
@@ -1,19 +1,159 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    CfnOutput,
+    Tags,
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    aws_autoscaling as autoscaling,
+    aws_elasticloadbalancingv2 as elbv2,
+    aws_certificatemanager as acm,
+    aws_route53 as route53,
+    aws_route53_targets as route53_targets,
 )
+import os
+import json
 from constructs import Construct
+import aws_cdk as cdk
 
-class CdkPythonAwsAutoscalingGroupStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+class CdkPythonAwsAutoscalingGroupStack(cdk.Stack):
 
-        # The code that defines your stack goes here
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkPythonAwsAutoscalingGroupQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Parse the TAGS environment variable
+        tags = json.loads(os.getenv('TAGS', '[]'))
+
+        print("Parsed tags:", tags)  # Debugging: log the tags to verify
+
+        # Apply tags to the stack
+        for tag in tags:
+            Tags.of(self).add(tag['key'], tag['value'])
+
+        # Get CREATE_DNS_RECORD environment variable
+        create_dns_record = os.getenv('CREATE_DNS_RECORD', 'false')
+
+        # Query the environment to get the current VPC
+        vpc = ec2.Vpc.from_lookup(self, 'ExistingVpc', is_default=True)
+
+        # Print the VPC ID and CIDR for debugging
+        print(f'Current VPC ID: {vpc.vpc_id}')
+        print(f'Current VPC CIDR: {vpc.vpc_cidr_block}')
+
+        # Get the subnets for the current VPC
+        subnets = vpc.select_subnets()
+
+        # Print the subnet IDs
+        for idx, subnet_id in enumerate(subnets.subnet_ids):
+            print(f'Subnet {idx + 1} ID: {subnet_id}')
+
+        # Create a security group
+        security_group = ec2.SecurityGroup(self, 'MySecurityGroup',
+                                           vpc=vpc,
+                                           security_group_name='MySecurityGroup')
+
+        # Add ingress/egress rules
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443), 'Allow HTTPS access')
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), 'Allow HTTP access')
+        security_group.add_egress_rule(ec2.Peer.any_ipv4(), ec2.Port.all_traffic(), 'Allow all traffic')
+
+        # Create an IAM role for the EC2 instance
+        role = iam.Role(self, 'MyInstanceRole',
+                        assumed_by=iam.ServicePrincipal('ec2.amazonaws.com'),
+                        role_name='MyInstanceRole',
+                        description='Allows EC2 instances to call AWS services on your behalf')
+
+        # Attach an IAM policy to the role
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name('AmazonSSMManagedInstanceCore'))
+
+        # VPC Endpoints for SSM
+        ec2.InterfaceVpcEndpoint(self, 'SSMVPCEndpoint', vpc=vpc, service=ec2.InterfaceVpcEndpointAwsService.SSM)
+        ec2.InterfaceVpcEndpoint(self, 'EC2MessagesVPCEndpoint', vpc=vpc, service=ec2.InterfaceVpcEndpointAwsService.EC2_MESSAGES)
+        ec2.InterfaceVpcEndpoint(self, 'SSMSessionManagerVPCEndpoint', vpc=vpc, service=ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES)
+
+        # User data script
+        user_data = ec2.UserData.for_linux()
+        user_data.add_commands(
+            '#!/bin/bash',
+            'dnf update -y',
+            'dnf install -y httpd',
+            'systemctl start httpd',
+            'systemctl enable httpd',
+            'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html'
+        )
+
+        # Launch template
+        launch_template = ec2.LaunchTemplate(self, 'MyLaunchTemplate',
+                                             machine_image=ec2.MachineImage.latest_amazon_linux2023(),
+                                             instance_type=ec2.InstanceType('t2.micro'),
+                                             user_data=user_data,
+                                             security_group=security_group,
+                                             role=role,
+                                             associate_public_ip_address=True)
+
+        # Auto scaling group
+        auto_scaling_group = autoscaling.AutoScalingGroup(self, 'MyAutoScalingGroup',
+                                                          vpc=vpc,
+                                                          vpc_subnets=ec2.SubnetSelection(subnets=subnets.subnets),  # Pass the subnets this way
+                                                          launch_template=launch_template,
+                                                          min_capacity=1,
+                                                          max_capacity=3,
+                                                          )
+
+        # Load balancer
+        load_balancer = elbv2.ApplicationLoadBalancer(self, 'MyLoadBalancer',
+                                                      vpc=vpc,
+                                                      internet_facing=True)
+
+        # Target group
+        target_group = elbv2.ApplicationTargetGroup(self, 'MyTargetGroup',
+                                                    vpc=vpc,
+                                                    port=80,
+                                                    protocol=elbv2.ApplicationProtocol.HTTP,
+                                                    target_type=elbv2.TargetType.INSTANCE)
+
+        if create_dns_record == 'true':
+            # Route53 hosted zone and certificate
+            hosted_zone = route53.HostedZone.from_hosted_zone_attributes(self, 'MyHostedZone',
+                                                                         hosted_zone_id=os.getenv('HOSTED_ZONE_ID', ''),
+                                                                         zone_name=os.getenv('ZONE_NAME', ''))
+
+            cert = acm.Certificate(self, 'MyCertificate',
+                                   domain_name=os.getenv('DNS_NAME', ''),
+                                   validation=acm.CertificateValidation.from_dns(hosted_zone))
+
+            # DNS A record
+            route53.ARecord(self, 'AliasRecord',
+                            zone=hosted_zone,
+                            target=route53.RecordTarget.from_alias(route53_targets.LoadBalancerTarget(load_balancer)),
+                            record_name=os.getenv('DNS_NAME', ''))
+
+            # HTTPS listener
+            listener = load_balancer.add_listener('MyListener', port=443, open=True, certificates=[cert])
+            load_balancer.add_listener('RedirectListener', port=80, open=True,
+                                       default_action=elbv2.ListenerAction.redirect(protocol='HTTPS', port='443', permanent=True))
+
+            # Attach target groups
+            listener.add_target_groups('MyTargetGroup', target_groups=[target_group])
+            target_group.add_target(auto_scaling_group)
+        else:
+            # HTTP listener
+            listener = load_balancer.add_listener('MyListener', port=80, open=True)
+            listener.add_target_groups('MyTargetGroup', target_groups=[target_group])
+            target_group.add_target(auto_scaling_group)
+
+        # Output values
+        CfnOutput(self, 'LoadBalancerDNS', value=load_balancer.load_balancer_dns_name)
+        CfnOutput(self, 'LoadBalancerARN', value=load_balancer.load_balancer_arn)
+        CfnOutput(self, 'TargetGroupARN', value=target_group.target_group_arn)
+        CfnOutput(self, 'AutoScalingGroupARN', value=auto_scaling_group.auto_scaling_group_arn)
+        CfnOutput(self, 'AutoScalingGroupName', value=auto_scaling_group.auto_scaling_group_name)
+
+
+app = cdk.App()
+
+# Define the AWS account and region for the stack environment
+CdkPythonAwsAutoscalingGroupStack(app, "CdkPythonAwsAutoscalingGroupStack", env={
+    'account': os.getenv('CDK_DEFAULT_ACCOUNT'),
+    'region': os.getenv('CDK_DEFAULT_REGION')
+})
+app.synth()
